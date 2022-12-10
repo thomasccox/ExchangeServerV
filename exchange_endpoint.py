f7e1cf60db7843aad98daba737f8e61c000f9d64
Thomas Cox <94573474+thomasccox@users.noreply.github.com>
2022-12-10 12:08:24 -0500

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -231,7 +231,6 @@ def find_match(order):
   
 @app.route('/address', methods=['POST'])
 def address():
-    print("TEST")
     if request.method == "POST":
         content = request.get_json(silent=True)
         if 'platform' not in content.keys():
