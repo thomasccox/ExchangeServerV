98e000535bff9c996d4fd86dce8b56e906682ec5
Thomas Cox <94573474+thomasccox@users.noreply.github.com>
2022-12-13 22:06:38 -0500

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -210,7 +210,7 @@ def execute_txes(txes):
     join_txs = eth_txs + algo_txs
     #       2. Add all transactions to the TX table
     for i,tx in enumerate(join_txs):
-        tx_objTX(platform = tx['platform'], receiver_pk = tx['receiver_pk'], order_id = tx['order_id'], order = tx['order'])
+        tx_obj = TX(platform = tx['platform'], receiver_pk = tx['receiver_pk'], order_id = tx['order_id'], order = tx['order'])
         g.session.add(tx_obj)
         g.session.commit()
 
